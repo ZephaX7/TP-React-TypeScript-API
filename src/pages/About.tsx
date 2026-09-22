8362b707721f29290bd5feba23cd8c26a4a08e05
Theo <theo.delourneau@gmail.com>
2026-09-22 11:09:28 +0200

    suggestion + css

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,19 +1,29 @@
 const About = () => {
   return (
-    <header>
-      <h1>A propos</h1>
-      
-    
-    <h2>Rick & Morty Explorer</h2>
-    <p>
-      Ce projet est un catalogue de personnages de la série animée Rick et Morty. Il permet de consulter les informations principales de chaque personnage, de les ajouter à vos favoris et de proposer de nouveaux personnages.
-    </p>
-    <p>
-      Les données sont récupérées via l’API officielle de Rick et Morty. Ce projet est réalisé dans le cadre d’un TP React avec TypeScript
-    </p>
+    <section className="page">
+      <h2>À propos</h2>
 
-    </header>
-  );
-};
+      <p>
+        Découvrez quelques informations sur le projet Rick & Morty Explorer.
+      </p>
 
-export default About;
+      <article className="about-card">
+        <h3>Rick & Morty Explorer</h3>
+
+        <p>
+          Ce projet est un catalogue de personnages de la série animée
+          Rick et Morty. Il permet de consulter les informations principales
+          de chaque personnage, de les ajouter à vos favoris et de proposer
+          de nouveaux personnages.
+        </p>
+
+        <p>
+          Les données sont récupérées via l'API Rick and Morty.
+          Ce projet est réalisé dans le cadre d'un TP React avec TypeScript.
+        </p>
+      </article>
+    </section>
+  )
+}
+
+export default About
